5cf861c728386f90015ec5d2a9a9059255e0d57b
shch989 <tankpop11@gmail.com>
2023-01-09 21:57:06 +0900

    유효성 체크

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,15 +1,43 @@
+const { validationResult } = require('express-validator')
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }],
+    posts: [
+      {
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: '../images/dice.png',
+        creator: {
+          name: '성 현',
+        },
+        date: new Date(),
+      },
+    ],
   })
 }
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message: 'Validation failed, entered data is incorrect.',
+        errors: errors.array(),
+      })
+  }
   const title = req.body.title
   const content = req.body.content
 
   res.status(201).json({
     message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content },
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: '성현' },
+      createdAt: new Date(),
+    },
   })
 }
--- routes/feed.js
+++ routes/feed.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const { body } = require('express-validator')
 
 const feedController = require('../controllers/feed')
 
 const router = express.Router()
 
+// GET /feed/posts
 router.get('/posts', feedController.getPosts)
-router.post('/posts', feedController.createPost)
+
+// POST /feed/post
+router.post(
+  '/posts',
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 }),
+  ],
+  feedController.createPost
+)
 
 module.exports = router
