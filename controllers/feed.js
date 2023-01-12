aa94814edd87787463cf380a9343ff9f781844c9
shch989 <tankpop11@gmail.com>
2023-01-12 17:10:45 +0900

    socket io 마무리

--- controllers/feed.js
+++ controllers/feed.js
@@ -14,6 +14,7 @@ exports.getPosts = async (req, res, next) => {
     const totalItems = await Post.find().countDocuments()
     const posts = await Post.find()
       .populate('creator')
+      .sort({ createdAt: -1 })
       .skip((currentPage - 1) * perPage)
       .limit(perPage)
 
@@ -135,13 +136,13 @@ exports.updatePost = async (req, res, next) => {
     throw error
   }
   try {
-    const post = await Post.findById(postId)
+    const post = await Post.findById(postId).populate('creator')
     if (!post) {
       const error = new Error('Could not find post.')
       error.statusCode = 404
       throw error
     }
-    if (post.creator.toString() !== req.userId) {
+    if (post.creator._id.toString() !== req.userId) {
       const error = new Error('Not authorized!')
       error.statusCode = 403
       throw error
@@ -153,6 +154,7 @@ exports.updatePost = async (req, res, next) => {
     post.imageUrl = imageUrl
     post.content = content
     const result = await post.save()
+    io.getIO().emit('posts', { action: 'update', post: result })
     res.status(200).json({ message: 'Post updated!', post: result })
   } catch (err) {
     if (!err.statusCode) {
@@ -184,7 +186,7 @@ exports.deletePost = async (req, res, next) => {
     const user = await User.findById(req.userId)
     user.posts.pull(postId)
     await user.save()
-
+    io.getIO().emit('posts', { action: 'delete', post: postId })
     res.status(200).json({ message: 'Deleted post.' })
   } catch (err) {
     if (!err.statusCode) {
